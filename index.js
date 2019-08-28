9e050e5067a54d82511a46093cda0ef7badefae2
Bryson <bryson.gariepy@gmail.com>
2019-08-27 21:02:42 -0400

    Initial

---
+++ index.js
@@ -0,0 +1,58 @@
+const Discord = require('discord.js');
+const {Client, Attachment} = require('discord.js');
+const {prefix, token} = require('./config.json');
+const {version, author} = require('./package.json');
+const client = new Discord.Client();
+
+client.once('ready', () => {
+    console.log('Ready!');
+    client.user.setActivity(`code guides. Ver:${version}`, {type: 'WATCHING'}).catch(console.error);
+})
+
+client.on('message', message=> {
+
+    let args = message.content.substring(`${prefix}`.length).split(" ");
+
+    switch (args[0]){
+        case 'ping':
+            message.reply('pong!');
+            //message.channel.send for just saying without @ing
+        break;
+        case 'info':
+            if(args[1] === 'version'){
+                message.channel.send(`Currently, I am on version ${version}`)
+            }else{
+                message.reply(`I was made by ${author}`)
+            }
+        break;
+        case 'version':
+            message.channel.send(`Currently, I am on version ${version}`)
+        break;
+        case 'clear':
+            if(!args[1]) return message.reply('Please define how many messages you want cleared!')
+            if(!message.member.hasPermission('MANAGE_MESSAGES')) return message.reply('Sorry, you need to have permisson: MANAGE_MESSAGES. :frown:')
+            message.channel.bulkDelete(args[1]);
+        break;
+        case 'embedtest':
+            const embed = new Discord.RichEmbed()
+            .setColor(0x5eeb34) //Hex color requires '0x' and then the number
+            .setTitle('Discord Information:')
+            .addField('Username', message.author.username)
+            .setThumbnail(message.author.avatarURL)
+            .setFooter('Discord Embed Test')
+            message.channel.send(embed);
+        break;
+        case '4head':
+            const attachment = new Attachment('https://www.sccpre.cat/png/big/13/139450_4head-png.png')
+            message.channel.send(message.author, attachment);
+        break;
+        case 'vote':
+            const pp = new Object()
+            message.channel.send('Please react yes/no!')
+            message.react('✅')
+            message.react('❌')
+        break;
+    }
+})
+
+client.login(token);
