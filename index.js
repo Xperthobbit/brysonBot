598f49d04dd63f25c5df32b71b746f0c70b53c28
Bryson Gariepy <Xperthobbit@users.noreply.github.com>
2019-09-13 19:35:19 -0400

    updated to 1.1

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ client.once('ready', () => {
     console.log('Established connection successfully.');
     console.log(' ');
     console.log('Bot name: ' + client.user.tag);
-    client.user.setActivity(`code guides. Ver:${version}`, {type: 'WATCHING'}).catch(console.error);
+    client.user.setActivity(`code guides. v${version}`, {type: 'WATCHING'}).catch(console.error);
     console.log("Servers deployed in:")
     client.guilds.forEach((guild) => {
         console.log(" - " + guild.name)
@@ -42,18 +42,21 @@ client.on('message', message=> {
                 message.reply(`I was made by ${author}! My Github repo is: https://github.com/Xperthobbit/brysonBot`)
             }
         break;
+        //Version functions
         case 'github':
             message.reply('Here is my git repo: https://github.com/Xperthobbit/brysonBot')
         break;
         case 'version':
             message.channel.send(`Currently, I am on version ${version}`)
         break;
+        //Clearing chat function 
         case 'clear':
             if(!args[1]) return message.reply('Please define how many messages you want cleared!')
             if(!message.member.hasPermission('MANAGE_MESSAGES')) return message.reply('Sorry, you need to have permisson: MANAGE_MESSAGES. :frown:')
             //if(!message.member.roles.find(r => r.name === "Role_name_here")) return message.reply('Sorry, you need to have the role: "role_name_here". :frown:')
             message.channel.bulkDelete(args[1]);
         break;
+        //Testing the embed func
         case 'embedtest':
             const embed = new Discord.RichEmbed()
             .setColor(0x5eeb34) //Hex color requires '0x' and then the number
@@ -63,15 +66,21 @@ client.on('message', message=> {
             .setFooter('Discord Embed Test')
             message.channel.send(embed);
         break;
+        //Print 4Head emote function
         case '4head':
             const attachment = new Attachment('https://www.sccpre.cat/png/big/13/139450_4head-png.png')
             message.channel.send(message.author, attachment);
         break;
+        //Voting function
         case 'vote':
             const pp = new Object()
             message.channel.send('Please react ✅ or ❌ to the following question!')
             message.react('✅')
             message.react('❌')
+        break;
+        case 'CPA':
+            let Role = message.guild.roles.find(role => role.name === "CPA");
+            message.member.addRole(Role);
         break;
     }
 })
